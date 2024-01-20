77485076676e0546d981b490ed8b71a9a22f1d33
hanie-ft <hanie.ft95@gmail.com>
2024-01-20 08:02:04 +0330

    Add authentication

--- django_practices/booklovers/booklovers/api/urls.py
+++ django_practices/booklovers/booklovers/api/urls.py
@@ -1,16 +1,7 @@
-# from django.urls import path, include
-# from booklovers.books.apis.books import BookApi
-# from booklovers.users.apis.users import RegisterUserApi
-
-# urlpatterns = [
-#     path("book/", BookApi.as_view(), name="book"),
-#     path('users/', RegisterUserApi.as_view, name='users'),
-#     # path('auth/', include(('booklovers.authentication.urls', 'auth'))),
-# ]
-
 from django.urls import path, include
 
 urlpatterns = [
     path("books/", include(("booklovers.books.urls", "books"))),
     path("users/", include(("booklovers.users.urls", "users"))),
+    path("auth/", include(("booklovers.authentication.urls", "auth"))),
 ]
